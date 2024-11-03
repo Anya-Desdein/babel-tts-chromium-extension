326c4b3f7aa98aedc0f0020c1b19e7fc12f97bc6
Anya Desdein <heavelyn1999@gmail.com>
2024-11-03 19:39:21 +0100

    Check for changes in saved TTS Input

--- scripts/background.js
+++ scripts/background.js
@@ -1,10 +1,27 @@
-let savedTTSInput = '';
+function checkForChanges() {
+  if (previousValue == savedTtsInput) {
+    return;
+  }
+
+  console.log('Value changed from', previousValue, 'to', savedTtsInput);
+  previousValue = savedTtsInput;
+
+  
+
+}
+
+let previousValue = '';
+let savedTtsInput = '';
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (!(request.action === 'babel_tts_save_text_input')){
     return;
   }
 
-        savedTTSInput = request.value;
+        savedTtsInput = request.value;
         sendResponse({ status: 'success' });
 });
+
+
+const interval = 5000;
+setInterval(checkForChanges, interval);
