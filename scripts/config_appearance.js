fe84fddd1952059c42e633b507d93d258f85fa88
Anya Desdein <heavelyn1999@gmail.com>
2024-11-11 17:39:45 +0100

    Add theme picker
    BUT it works only on the subpage of my choice
    TODO:
    - add no wallpaper option
    - make it work globally without reloading

--- scripts/config_appearance.js
+++ scripts/config_appearance.js
@@ -1,9 +1,59 @@
+function getImageURL(fileName) {
+    return chrome.runtime.getURL(`themes/${fileName}`);
+}
 
-async function waitForDom() {
-    document.addEventListener('DOMContentLoaded', async function() {
-        createRouterListener("returnHome", "home.html");
-        createRouterListener("configHome", "config_home.html");
+function isImage(fileName) {
+    return /\.(jpg|jpeg|png|gif|svg)$/i.test(fileName);
+}
+async function createImageArray(themeList) {
+    for (let i = 1; i <= 20; i++) {
+        const imageName = "bg" + i + ".png";
+        const imageUrl = getImageURL(imageName);
+        try {
+            const response = await fetch(imageUrl, { method: 'HEAD' });
+            if (response.ok) {
+                themeList.push(imageUrl);
+            } else {
+                break;
+            }
+        } catch (error) {
+            break;
+        }
+    }
+}
+
+function addElementsToSelect(elementArray, selectName) {
+    const select = document.getElementById(selectName);
+    for (let i = 1; i <= elementArray.length; i++) {
+        const wallpaperName = elementArray[i-1].split('/').pop();
+        const newElement = new Option(wallpaperName, wallpaperName);
+        select.add(newElement);
+    }
+}
+
+function addListenerForThemeChange(themeList) {
+    const pickWallpaperSaveButton = document.getElementById("pickWallpaperSaveButton");
+
+    pickWallpaperSaveButton.addEventListener('click', function() {
+        const wallpaperName = document.getElementById('pickWallpaper').value;
+        console.log(wallpaperName);
+
+        const pickedWallpaper = themeList.find(url => url.endsWith(wallpaperName));
+        console.log("wallpaper: ", pickedWallpaper);
+        document.documentElement.style.setProperty('--dynamic-background-image', `url(${pickedWallpaper})`);
     });
 }
 
-waitForDom()
+async function waitForDom() {
+    themeList = [];
+    document.addEventListener('DOMContentLoaded', async function() {
+        createRouterListener("returnHome", "home.html");
+        createRouterListener("configHome", "config_home.html");
+
+        await createImageArray(themeList);
+        addElementsToSelect(themeList, "pickWallpaper");
+        addListenerForThemeChange(themeList);
+    });
+}
+
+waitForDom();
